a422e4b7aa357619274391fca5b7c91486145631
ssu-sw <ssu-sw@21-305-21>
2025-11-03 13:36:04 +0900

    초기 커밋.

---
+++ EscapeView.cpp
@@ -0,0 +1,128 @@
+﻿
+// EscapeView.cpp: CEscapeView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Escape.h"
+#endif
+
+#include "EscapeDoc.h"
+#include "EscapeView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CEscapeView
+
+IMPLEMENT_DYNCREATE(CEscapeView, CView)
+
+BEGIN_MESSAGE_MAP(CEscapeView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CEscapeView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CEscapeView 생성/소멸
+
+CEscapeView::CEscapeView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CEscapeView::~CEscapeView()
+{
+}
+
+BOOL CEscapeView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CEscapeView 그리기
+
+void CEscapeView::OnDraw(CDC* /*pDC*/)
+{
+	CEscapeDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CEscapeView 인쇄
+
+
+void CEscapeView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CEscapeView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CEscapeView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CEscapeView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+void CEscapeView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CEscapeView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CEscapeView 진단
+
+#ifdef _DEBUG
+void CEscapeView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CEscapeView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CEscapeDoc* CEscapeView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CEscapeDoc)));
+	return (CEscapeDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CEscapeView 메시지 처리기
