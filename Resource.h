822673d8d79fb81311c705d101937ee6e102dce3
ssu-sw <ssu-sw@21-305-22>
2025-11-10 13:41:34 +0900

    BookListDlg, BookDlg class 추가

--- Resource.h
+++ Resource.h
@@ -8,15 +8,16 @@
 #define IDD_ESCAPE_DIALOG               102
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG_BOOKLIST             129
+#define IDD_DIALOG_                     131
+#define IDD_DIALOG_BOOK                 131
 #define IDC_BUTTON_WPBOOK               1000
-#define IDC_BUTTON2                     1001
 #define IDC_BUTTON_CBOOK                1001
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
