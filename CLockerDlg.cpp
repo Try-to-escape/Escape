cb744661c0bb2545a2e735d53cf573f31e40d56f
Garam100 <backgaram123@gmail.com>
2025-11-10 14:08:35 +0900

    feat: 금고 이미지 추가

---
+++ CLockerDlg.cpp
@@ -0,0 +1,64 @@
+﻿// CLockerDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Escape.h"
+#include "afxdialogex.h"
+#include "CLockerDlg.h"
+
+
+// CLockerDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CLockerDlg, CDialogEx)
+
+CLockerDlg::CLockerDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_LockerDlg, pParent)
+{
+
+}
+
+CLockerDlg::~CLockerDlg()
+{
+}
+
+void CLockerDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CLockerDlg, CDialogEx)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+
+//초기화
+BOOL CLockerDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	HRESULT hr = m_imgLocker.Load(L"res/locker.bmp");
+	if (FAILED(hr))
+	{
+		CString msg;
+		msg.Format(L"이미지 로드 실패! HRESULT=0x%08X", hr);
+		AfxMessageBox(msg);
+	}
+	Invalidate();
+
+	return TRUE;
+	
+}
+
+//화면 출력
+void CLockerDlg::OnPaint()
+{
+	CPaintDC dc(this);
+
+	//금고 이미지 출력
+	if (m_imgLocker) {
+		m_imgLocker.Draw(dc, 0, 0);
+	}
+
+}
---
+++ CLockerDlg.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CLockerDlg 대화 상자
+
+class CLockerDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CLockerDlg)
+
+private:
+	CImage m_imgLocker;
+
+public:
+	CLockerDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CLockerDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LockerDlg };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+};
