667b8a034454a1c1f5e765a97ed473d87d87a5fd
ssu-sw <ssu-sw@21-305-22>
2025-11-11 11:24:04 +0900

    make Book, BookList Dlg class

---
+++ CBookDlg.cpp
@@ -0,0 +1,34 @@
+﻿// CBookDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Escape.h"
+#include "afxdialogex.h"
+#include "CBookDlg.h"
+
+
+// CBookDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CBookDlg, CDialogEx)
+
+CBookDlg::CBookDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG_BOOK, pParent)
+{
+
+}
+
+CBookDlg::~CBookDlg()
+{
+}
+
+void CBookDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CBookDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CBookDlg 메시지 처리기
