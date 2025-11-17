bae71860ed3d02d358d0bc1be5c798be8ca162b4
ssu-sw <ssu-sw@21-305-22>
2025-11-17 13:30:55 +0900

    booklist bmp파일 추가

--- Resource.h
+++ Resource.h
@@ -9,7 +9,7 @@
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG_BOOKLIST             129
 #define IDD_DIALOG_BOOK                 130
-#define IDB_BITMAP1                     133
+#define IDB_BITMAP_BOOKLIST             133
 #define IDC_BUTTON_FBOOK                1000
 #define IDC_BUTTON_WBOOK                1001
 
@@ -17,7 +17,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
