6bf88daef05fbe37fcdd3d9c078e825af73755cd
Garam100 <backgaram123@gmail.com>
2025-11-17 14:46:10 +0900

    feat : 교수님 힌트 대화상자

---
+++ CHintDlg.cpp
@@ -0,0 +1,102 @@
+﻿// CHintDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Escape.h"
+#include "afxdialogex.h"
+#include "CHintDlg.h"
+
+
+// CHintDlg 대화 상자
+
+IMPLEMENT_DYNAMIC(CHintDlg, CDialogEx)
+
+CHintDlg::CHintDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG_HINT, pParent)
+{
+
+}
+
+CHintDlg::~CHintDlg()
+{
+}
+
+void CHintDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_BUTTON_OUT, m_btnOut);
+}
+
+
+BEGIN_MESSAGE_MAP(CHintDlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_BN_CLICKED(IDC_BUTTON_OUT, &CHintDlg::OnBnClickedButtonOut)
+	ON_WM_TIMER()
+END_MESSAGE_MAP()
+
+
+// CHintDlg 메시지 처리기
+
+BOOL CHintDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	//1. 이미지들 불러오기
+	m_imgHint[0].Load(L"res/image/hint1.bmp");
+	m_imgHint[1].Load(L"res/image/hint2.bmp");
+	m_imgHint[2].Load(L"res/image/hint3.bmp");
+	m_imgHint[3].Load(L"res/image/hint4.bmp");
+	m_imgHint[4].Load(L"res/image/hint5.bmp");
+	m_imgHint[5].Load(L"res/image/hint6.bmp");
+	m_imgHint[6].Load(L"res/image/hint7.bmp");
+	m_imgHintFail.Load(L"res/image/hintFail.bmp");
+
+	//2. 초기 화면 설정
+	m_nCurrentImageIndex = 0;
+	m_pCurrentImage = &m_imgHint[m_nCurrentImageIndex];
+	Invalidate();
+
+	//3. 타이머 설정
+	SetTimer(1, 4000, NULL);
+
+	return TRUE;  
+}
+
+void CHintDlg::OnPaint()
+{
+	CPaintDC dc(this); 
+	if (m_pCurrentImage && !m_pCurrentImage->IsNull())
+	{
+		m_pCurrentImage->Draw(dc, 0, 0);
+	}
+}
+
+void CHintDlg::OnBnClickedButtonOut()
+{
+	if (m_nCurrentImageIndex >= 7) {
+		AfxMessageBox(_T("교수님에게 들키지 않았다! 계속 힌트를 찾아보자!"), MB_OK | MB_ICONINFORMATION);
+		EndDialog(IDOK);
+	}
+	else {
+		AfxMessageBox(_T("교수님께 들켜버렸다!"), MB_OK | MB_ICONWARNING);
+		m_pCurrentImage = &m_imgHintFail;
+		Invalidate();
+		//실패창 띄우기
+	}
+		
+}
+
+void CHintDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	m_nCurrentImageIndex++;
+
+	if (m_nCurrentImageIndex >= 7)
+		return;
+
+	m_pCurrentImage = &m_imgHint[m_nCurrentImageIndex];  
+
+	Invalidate();
+
+
+	CDialogEx::OnTimer(nIDEvent);
+}
---
+++ CHintDlg.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CHintDlg 대화 상자
+
+class CHintDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CHintDlg)
+
+public:
+	CHintDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CHintDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_HintDlg};
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CImage m_imgHint[7];
+	virtual BOOL OnInitDialog();
+	//나가기 버튼
+	CButton m_btnOut;
+	//화면 출력 이미지
+	CImage* m_pCurrentImage;
+	afx_msg void OnPaint();
+	afx_msg void OnBnClickedButtonOut();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+};
