351ea8940bc7c108eb7be8276e54aa57cb39c8c5
affossoffa <yeongrim9421@gmail.com>
2025-11-24 13:42:49 +0900

    resource 파일 변경

--- Resource.h
+++ Resource.h
@@ -3,26 +3,33 @@
 // Escape.rc에서 사용되고 있습니다.
 //
 #define IDM_ABOUTBOX                    0x0010
-#define IDD_ABOUTBOX                    100
 #define IDS_ABOUTBOX                    101
 #define IDD_ESCAPE_DIALOG               102
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG_START                129
-#define IDD_DIALOG_BOOKLIST             139
+#define IDB_BITMAP1                     144
+#define IDB_START                       145
+#define IDD_DIALOG_EXPLAIN              147
+#define IDB_HIDE                        149
+#define IDB_BITMAP2                     150
+#define IDB_EXPLAIN                     150
 #define IDC_BUTTON1                     1000
+#define IDB_BACKGROUND                  1000
+#define IDC_HIDE                        1000
 #define IDC_BUTTON_PICTURE              1002
 #define IDC_BUTTON_LOCKER               1003
-#define IDC_BUTTON4                     1004
 #define IDC_BUTTON_STAND                1004
 #define IDC_BUTTON_BOOKLIST             1005
+#define IDC_OUT                         1008
+#define IDC_STATIC_TIMER                1009
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        144
+#define _APS_NEXT_RESOURCE_VALUE        151
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
